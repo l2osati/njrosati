9d92d688944ccff78c4840d2610a5c9b5911d000
l2osati <njrosati@gmail.com>
2022-03-01 09:23:51 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='njrosati',
-    version='0.0.3',
+    version='0.0.4',
     author='Nick Rosati',
     author_email='l2osati@gmail.com',
     description='Testing installation of Package',
